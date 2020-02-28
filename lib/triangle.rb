1e657d545e5c21dd2a27afc67b5825985e6ceca6
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-28 07:12:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,12 +12,12 @@ class Triangle
     elsif side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
       raise TriangleError
     else
-      if side1 != side2 && side2 != side3 && side1 !=side3
-        :scalene
-      elsif side1 == side2 && side2 == side3
+      if side1 == side2 && side2 == side3
         :equilateral
       elsif side1 == side2 || side2 == side3 || side1 == side3
         :isosceles
+      else
+        :scalene
       end
     end
   end
